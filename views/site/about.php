37d9f677f3efe72f2bbf6d70e2dd5d5436e95f72
pk <pablo.kogan@fi.uncoma.edu.ar>
2020-10-19 12:28:25 -0300

    acerca de

--- views/site/about.php
+++ views/site/about.php
@@ -11,7 +11,10 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        Los repositorio de fuentes disponibles en:
+        Coco - Jorge Rodriguez. Idea y Concepto de Meta-enunciado.<br/>
+        Pablo Kogan. Desarrollador<br/>
+        Giuliano Marinelli, Juan De la Fuente, Gonzalo Heffesse y Daniel Dolz. Testers.<br/>
+        Fuentes disponibles en:
     </p>
 
     <code><a href="https://github.com/pkogan/huipa">https://github.com/pkogan/huipa</a></code>
